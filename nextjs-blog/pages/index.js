5427c4efbccdead9aaf20b9479513d3084065858
Fshiro <ceoff@bunjin.com>
2023-09-15 13:47:56 +0900

    2nd Commit

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -10,7 +10,10 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Learn MoreM<br></br> <a href="https://nextjs.org">Next.js!</a>
+          Read{' '}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
       </main>
     </div>
